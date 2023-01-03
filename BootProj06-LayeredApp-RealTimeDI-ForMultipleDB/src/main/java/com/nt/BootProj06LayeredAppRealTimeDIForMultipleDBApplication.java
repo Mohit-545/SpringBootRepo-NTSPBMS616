e6e0ce0ca8ad52a68cf36b1591511a7d3f92524c
DELL <DELL@192.168.0.103>
2023-01-03 13:23:48 +0530

    Sixth Project - First Commit

---
+++ BootProj06-LayeredApp-RealTimeDI-ForMultipleDB/src/main/java/com/nt/BootProj06LayeredAppRealTimeDIForMultipleDBApplication.java
@@ -0,0 +1,90 @@
+//BootProj06LayeredAppRealTimeDIForMultipleDBApplication.java
+package com.nt;
+
+import java.io.Closeable;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.Environment;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.nt.controller.PayrollOperationsController;
+import com.nt.model.Employee;
+
+@SpringBootApplication
+@ImportResource("com/nt/cfgs/applicationContext.xml")
+public class BootProj06LayeredAppRealTimeDIForMultipleDBApplication {
+//	@Bean("c3p0DS")
+//	public ComboPooledDataSource createC3P0DS() throws Exception{
+//		System.out.println("BootProj06LayeredAppRealTimeDIForMultipleDBApplication.createC3P0DS()");
+//		ComboPooledDataSource cdps = new ComboPooledDataSource();
+//		cdps.setDriverClass("com.mysql.cj.jdbc.Driver");
+//		cdps.setJdbcUrl("jdbc:mysql:///NTSPBMS616DB");
+//		cdps.setUser("root"); 
+//		cdps.setPassword("root");
+//		cdps.setInitialPoolSize(10);
+//		cdps.setMaxPoolSize(100);
+//		return cdps;
+//	}
+					// OR
+//	@Autowired
+//	private Environment env;
+//	
+//	@Bean("c3p0DS")
+//	public ComboPooledDataSource createC3P0DS() throws Exception{
+//		ComboPooledDataSource cdps = new ComboPooledDataSource();
+//		System.out.println("BootProj06LayeredAppRealTimeDIForMultipleDBApplication.createC3P0DS()");
+//		ComboPooledDataSource cpds = new ComboPooledDataSource();
+//		cpds.setDriverClass(env.getRequiredProperty("spring.datasource.driver-class-name"));
+//		cpds.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+//		cpds.setUser(env.getRequiredProperty("spring.datasource.username")); 
+//		cpds.setPassword(env.getRequiredProperty("spring.datasource.password"));
+//		cpds.setInitialPoolSize(Integer.parseInt(env.getRequiredProperty("c3p0.minSize")));
+//		cpds.setMaxPoolSize(Integer.parseInt(env.getRequiredProperty("c3p0.maxSize")));
+//		return cpds;
+//	}
+	
+	
+	public static void main(String[] args) {
+		//get employee data from client
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter Employee Name : ");
+		String eName = scn.nextLine();
+		System.out.print("Enter Employee Address : ");
+		String eAddress = scn.next();
+		System.out.print("Enter Employee Salary : ");
+		double basicSalary = scn.nextDouble();
+		
+		//create Employee class object and set values gathered from client
+		Employee emp = new Employee();
+		
+		//set the values
+		emp.setEName(eName); emp.setEAdd(eAddress); emp.setBasicSalary(basicSalary);
+
+		//get the IOC Container
+		ApplicationContext ctx = SpringApplication.run(BootProj06LayeredAppRealTimeDIForMultipleDBApplication.class, args);
+		
+		//get the Controller class obj
+		PayrollOperationsController proll = ctx.getBean("payrollController", PayrollOperationsController.class);
+		
+		//invoke the b.method
+		try {
+			String resMesg = proll.processEmployee(emp);
+			System.out.println(resMesg);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Internal Error --- Please try again!!!!!    "+e.getMessage());
+		}
+		//close the container
+		((ConfigurableApplicationContext) ctx).close();
+		
+	}//main
+
+}//class
---
+++ BootProj06-LayeredApp-RealTimeDI-ForMultipleDB/src/main/java/com/nt/dao/MySQLEmployeeDAOImpl.java
@@ -0,0 +1,52 @@
+//EmployeeDAOImpl.java
+package com.nt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nt.model.Employee;
+@Repository("empDAO-mysql")
+public class MySQLEmployeeDAOImpl implements IEmployeeDAO {
+	//String Query
+	private static final String Insert_Employee_Query = "INSERT INTO EMPLOYEE_INFO (ENAME,EADD,BASICSALARY,GROSSSALARY,NETSALARY) VALUES (?,?,?,?,?)";
+	
+	//injecting DataSource obj ref
+	@Autowired
+	private DataSource ds;
+	
+	@Override
+	public int insertEmployee(Employee emp) throws Exception {
+		System.out.println("Injected DataSource Object Class Name :: "+ds.getClass().getName());
+		int count = 0;
+		try(Connection con = ds.getConnection();
+				PreparedStatement ps = con.prepareStatement(Insert_Employee_Query)){
+			//add values to query param
+			if(ps!=null) {
+				ps.setString(1,emp.getEName());
+				ps.setString(2, emp.getEAdd());
+				ps.setDouble(3, emp.getBasicSalary());
+				ps.setDouble(4, emp.getGrossSalary());
+				ps.setDouble(5, emp.getNetSalary());
+			}
+			//execute the query
+			count = ps.executeUpdate();
+		}//try
+		catch (SQLException se) {
+			se.printStackTrace();
+			throw se;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		
+		return count;
+	}//method
+
+}//class
