8e57844e734f77f443267ca1b9a22d439715cac6
mohit <mohit@192.168.0.101>
2023-01-19 09:34:32 +0530

    Fifteenth Project - First Commit

---
+++ BootProj15-MoreOnProfiles/src/main/java/com/nt/dao/OracleEmployeeDAOImpl.java
@@ -0,0 +1,60 @@
+//OracleEmployeeDAOImpl.java
+package com.nt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import com.nt.model.Employee;
+@Repository("empDAO-oracle")
+@Profile({"uat","prod"})
+public class OracleEmployeeDAOImpl implements IEmployeeDAO {
+	
+	public OracleEmployeeDAOImpl() {
+		System.out.println("OracleEmployeeDAOImpl::0-param Constructor");
+		System.out.println();
+	}
+	
+	//String Query
+	private static final String Insert_Employee_Query = "INSERT INTO EMPLOYEE_INFO VALUES (ENO_ID.NEXTVAL,?,?,?,?,?)";
+	
+	//injecting DataSource obj ref
+	@Autowired
+	private DataSource ds;
+	
+	@Override
+	public int insertEmployee(Employee emp) throws Exception {
+		System.out.println("Injected DataSource Object Class Name :: "+ds.getClass().getName());
+		int count = 0;
+		try(Connection con = ds.getConnection();
+				PreparedStatement ps = con.prepareStatement(Insert_Employee_Query)){
+			//add values to query param
+			if(ps!=null) {
+				ps.setString(1,emp.getEName());
+				ps.setString(2, emp.getEAdd());
+				ps.setDouble(3, emp.getBasicSalary());
+				ps.setDouble(4, emp.getGrossSalary());
+				ps.setDouble(5, emp.getNetSalary());
+			}
+			//execute the query
+			count = ps.executeUpdate();
+		}//try
+		catch (SQLException se) {
+			se.printStackTrace();
+			throw se;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		
+		return count;
+	}//method
+
+}//class
