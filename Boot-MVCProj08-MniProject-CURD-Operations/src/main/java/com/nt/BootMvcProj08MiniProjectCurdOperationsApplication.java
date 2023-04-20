9e2995918fb6907271d32e5fba0601a491984fea
mohit <mohit@192.168.0.102>
2023-04-20 12:34:16 +0530

    project name corrections performed

--- Boot-MVCProj08-MniProject-CURD-Operations/src/main/java/com/nt/BootMvcProj08MiniProjectCurdOperationsApplication.java
+++ Boot-MVCProj08-MniProject-CURD-Operations/src/main/java/com/nt/BootMvcProj08MiniProjectCurdOperationsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootMvcProj08MniProjectCurdOperationsApplication {
+public class BootMvcProj08MiniProjectCurdOperationsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootMvcProj08MniProjectCurdOperationsApplication.class, args);
+		SpringApplication.run(BootMvcProj08MiniProjectCurdOperationsApplication.class, args);
 	}
 
 }
